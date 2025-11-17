16b154db12aa6a7979c9467bd0ab52a40640021d
brahayan32 <brahayancampo32@gmail.com>
2025-11-17 17:23:10 -0500

    actualizado

--- src/main/java/com/campo/banco/model/TransferenciaEntity.java
+++ src/main/java/com/campo/banco/model/TransferenciaEntity.java
@@ -1,10 +1,14 @@
 package com.campo.banco.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table("transferencias")
 public class TransferenciaEntity {
     @Id
